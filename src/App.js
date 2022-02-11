1583f10c8fe575969c855e2e75027aa202e3452f
elcinp <elcinpars1@gmail.com>
2022-02-11 13:08:33 +0300

    handleSubmit function added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import data from "./data";
+
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    setText(data);
   };
 
   return (
@@ -26,14 +27,9 @@ function App() {
         </button>
       </form>
       <article className="lorem-text">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-          nesciunt!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-          nesciunt!
-        </p>
+        {text.map((item, index) => {
+          return <p key="index"> {item}</p>;
+        })}
       </article>
     </section>
   );
